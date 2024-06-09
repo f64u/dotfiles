af0b37ad71f6b0fd30b1ad9fdf9b9dc39cd792a1
Fady Adal <2masadel@gmail.com>
2024-06-09 16:35:54 -0500

    make it a patch

--- modules/darwin/sketchybar/default.nix
+++ modules/darwin/sketchybar/default.nix
@@ -1,37 +1,7 @@
 { pkgs, config, ... }:
 let
-  colors = ''
-    return {
-        black = 0xff181926,
-        white = 0xffcad3f5,
-        red = 0xffed8796,
-        green = 0xffa6da95,
-        blue = 0xff8aadf4,
-        yellow = 0xffeed49f,
-        orange = 0xfff5a97f,
-        magenta = 0xffc6a0f6,
-        grey = 0xff939ab7,
-        transparent = 0x00000000,
-
-        bar = {
-          bg = 0xff1e1e2e,
-          border = 0xff494d64,
-        },
-        popup = {
-          bg = 0xff1e1e2e,
-          border = 0xffcad3f5
-        },
-        bg1 = 0x603c3e4f,
-        bg2 = 0x60494d64,
-
-        with_alpha = function(color, alpha)
-          if alpha > 1.0 or alpha < 0.0 then return color end
-          return (color & 0x00ffffff) | (math.floor(alpha * 255.0) << 24)
-        end,
-      }
-  '';
   sketchybarConfig = pkgs.stdenv.mkDerivation {
-    name = "sketchybar-config";
+    name = "sketchybar-config-v0.1.0";
     src = pkgs.fetchFromGitHub {
       owner = "FelixKratz";
       repo = "dotfiles";
@@ -43,8 +13,8 @@ let
       SkyLight
     ];
     buildPhase = ''
+      patch -p0 -d ./.config/sketchybar -i ${./config.patch}
       make -C ./.config/sketchybar/helpers
-      echo "${colors}" > ./.config/sketchybar/colors.lua
     '';
     installPhase = ''
       cp -r ./.config/sketchybar $out
