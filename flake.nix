2f50910a4b390231ae437ad11480c26338b195b2
Fady Adal <2masadel@gmail.com>
2024-07-11 18:22:28 -0500

    switch to lix

--- flake.nix
+++ flake.nix
@@ -4,6 +4,11 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.90.0.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager = {
       inputs.nixpkgs.follows = "nixpkgs";
       url = "github:nix-community/home-manager/master";
@@ -17,7 +22,7 @@
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
   };
 
-  outputs = { self, nix-darwin, nix-homebrew, home-manager, ... }@inputs:
+  outputs = { self, nix-darwin, nix-homebrew, home-manager, lix-module, ... }@inputs:
     {
       # Build darwin flake using:
       # $ darwin-rebuild build --flake .#Fadys-MacBook-Pro
@@ -34,6 +39,7 @@
               autoMigrate = true;
             };
           }
+          lix-module.nixosModules.default
 
           ./hosts/darwin
         ];
