cb29fc42f2d6e53aa17ff89b94884464399dbddb
Fady Adal <2masadel@gmail.com>
2024-07-28 15:36:17 -0500

    update

--- modules/shared/programs/neovim/config/lua/custom/plugins/coq.lua
+++ modules/shared/programs/neovim/config/lua/custom/plugins/coq.lua
@@ -7,7 +7,10 @@ return {
       opts = {
         lsp = {
           on_attach = require('custom.util.lspconfig').on_attach,
-          autostart = true
+          init_options = {
+            show_notices_as_diagnostics = true,
+          },
+          autostart = false,
         }
       }
     }
