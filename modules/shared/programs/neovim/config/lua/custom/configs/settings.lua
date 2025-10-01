fb2c28e63be4fe926e5b8058be32a08efed3aefd
Fady Adal <2masadel@gmail.com>
2025-10-01 12:29:50 -0500

    fix(nvim): folding

--- modules/shared/programs/neovim/config/lua/custom/configs/settings.lua
+++ modules/shared/programs/neovim/config/lua/custom/configs/settings.lua
@@ -53,7 +53,7 @@ vim.opt.shortmess:append 'sI'
 
 -- Treesitter folding
 vim.opt.foldmethod = 'expr'
-vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 vim.opt.foldlevelstart = 99
 
 -- always show tabline
